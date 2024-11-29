eef8754980c9a50b0c50841679394859ce1e0235
Wilma Borghi <w.b29@outlook.com>
2024-11-28 22:32:03 -0300

    Configuração CORS

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 IMDB_API_URL = "https://imdb-com.p.rapidapi.com"
 headers = {
